ea0498335643527c9c831ec9834bdf4620f0d1e9
Nathan Hood <nathanjhood@googlemail.com>
2022-05-31 16:23:09 +0200

    Refactoring; also now calling unit delay by ref!

--- Source/Biquads.cpp
+++ Source/Biquads.cpp
@@ -128,23 +128,8 @@ void Biquads<SampleType>::prepare(juce::dsp::ProcessSpec& spec)
 template <typename SampleType>
 void Biquads<SampleType>::reset(SampleType initialValue)
 {
-    for (auto& wn_1 : Wn_1)
-        wn_1 = initialValue;
-
-    for (auto& wn_2 : Wn_2)
-        wn_2 = initialValue;
-
-    for (auto& xn_1 : Xn_1)
-        xn_1 = initialValue;
-
-    for (auto& xn_2 : Xn_2)
-        xn_2 = initialValue;
-
-    for (auto& yn_1 : Yn_1)
-        yn_1 = initialValue;
-
-    for (auto& yn_2 : Yn_2)
-        yn_2 = initialValue;
+    for (auto v : { &Wn_1, &Wn_2, &Xn_1, &Xn_2, &Yn_1, &Yn_2 })
+        std::fill(v->begin(), v->end(), initialValue);
 
     frq.reset(sampleRate, rampDurationSeconds);
     res.reset(sampleRate, rampDurationSeconds);
@@ -178,15 +163,15 @@ SampleType Biquads<SampleType>::processSample(int channel, SampleType inputValue
 template <typename SampleType>
 SampleType Biquads<SampleType>::directFormI(int channel, SampleType inputValue)
 {
+    auto& Xn1 = Xn_1[(size_t)channel];
+    auto& Xn2 = Xn_2[(size_t)channel];
+    auto& Yn1 = Yn_1[(size_t)channel];
+    auto& Yn2 = Yn_2[(size_t)channel];
+
     SampleType Xn = inputValue;
 
-    SampleType Yn = ((Xn * b0_) + (Xn_1[(size_t)channel] * b1_) + (Xn_2[(size_t)channel] * b2_) + (Yn_1[(size_t)channel] * a1_) + (Yn_2[(size_t)channel] * a2_));
-
-    Xn_2[(size_t)channel] = Xn_1[(size_t)channel];
-    Xn_1[(size_t)channel] = Xn;
-
-    Yn_2[(size_t)channel] = Yn_1[(size_t)channel];
-    Yn_1[(size_t)channel] = Yn;
+    SampleType Yn = ((Xn * b0_) + ((Xn1)*b1_) + (Xn2 * b2_) + (Yn1 * a1_) + (Yn2 * a2_)); 
+    Xn2 = Xn1, Xn1 = Xn, Yn2 = Yn1, Yn1 = Yn;
 
     return Yn;
 }
@@ -194,13 +179,15 @@ SampleType Biquads<SampleType>::directFormI(int channel, SampleType inputValue)
 template <typename SampleType>
 SampleType Biquads<SampleType>::directFormII(int channel, SampleType inputValue)
 {
+    auto& Wn1 = Wn_1[(size_t)channel];
+    auto& Wn2 = Wn_2[(size_t)channel];
+
     SampleType Xn = inputValue;
 
-    SampleType Wn = (Xn + ((Wn_1[(size_t)channel] * a1_) + (Wn_2[(size_t)channel] * a2_)));
-    SampleType Yn = ((Wn * b0_) + (Wn_1[(size_t)channel] * b1_) + (Wn_2[(size_t)channel] * b2_));
+    SampleType Wn = (Xn + ((Wn1 * a1_) + (Wn2 * a2_)));
+    SampleType Yn = ((Wn * b0_) + (Wn1 * b1_) + (Wn2 * b2_));
 
-    Wn_2[(size_t)channel] = Wn_1[(size_t)channel];
-    Wn_1[(size_t)channel] = Wn;
+    Wn2 = Wn1, Wn1 = Wn;
 
     return Yn;
 }
@@ -208,15 +195,19 @@ SampleType Biquads<SampleType>::directFormII(int channel, SampleType inputValue)
 template <typename SampleType>
 SampleType Biquads<SampleType>::directFormITransposed(int channel, SampleType inputValue)
 {
+    auto& Wn1 = Wn_1[(size_t)channel];
+    auto& Wn2 = Wn_2[(size_t)channel];
+    auto& Xn1 = Xn_1[(size_t)channel];
+    auto& Xn2 = Xn_2[(size_t)channel];
+
     SampleType Xn = inputValue;
-    SampleType Wn = (Xn + Wn_2[(size_t)channel]);
-    SampleType Yn = ((Wn * b0_) + Xn_2[(size_t)channel]);
 
-    Xn_2[(size_t)channel] = ((Wn * b1_) + Xn_1[(size_t)channel]);
-    Xn_1[(size_t)channel] = (Wn * b2_);
+    SampleType Wn = (Xn + Wn2);
+    SampleType Yn = ((Wn * b0_) + Xn2);
 
-    Wn_2[(size_t)channel] = ((Wn * a1_) + Wn_1[(size_t)channel]);
-    Wn_1[(size_t)channel] = (Wn * a2_);
+    Xn2 = ((Wn * b1_) + Xn1), Xn1 = (Wn * b2_);
+
+    Wn2 = ((Wn * a1_) + Wn1), Wn1 = (Wn * a2_);
 
     return Yn;
 }
@@ -224,12 +215,15 @@ SampleType Biquads<SampleType>::directFormITransposed(int channel, SampleType in
 template <typename SampleType>
 SampleType Biquads<SampleType>::directFormIITransposed(int channel, SampleType inputValue)
 {
+    auto& Xn1 = Xn_1[(size_t)channel];
+    auto& Xn2 = Xn_2[(size_t)channel];
+
     SampleType Xn = inputValue;
 
-    SampleType Yn = ((Xn * b0_) + (Xn_2[(size_t)channel]));
+    SampleType Yn = ((Xn * b0_) + (Xn2));
 
-    Xn_2[(size_t)channel] = ((Xn * b1_) + (Xn_1[(size_t)channel]) + (Yn * a1_));
-    Xn_1[(size_t)channel] = ((Xn * b2_) + (Yn * a2_));
+    Xn2 = ((Xn * b1_) + (Xn1) + (Yn * a1_));
+    Xn1 = ((Xn * b2_) + (Yn * a2_));
 
     return Yn;
 }
@@ -460,23 +454,9 @@ void Biquads<SampleType>::coefficients()
 template <typename SampleType>
 void Biquads<SampleType>::snapToZero() noexcept
 {
-    for (auto& wn_1 : Wn_1)
-        juce::dsp::util::snapToZero(wn_1);
-
-    for (auto& wn_2 : Wn_2)
-        juce::dsp::util::snapToZero(wn_2);
-
-    for (auto& xn_1 : Xn_1)
-        juce::dsp::util::snapToZero(xn_1);
-
-    for (auto& xn_2 : Xn_2)
-        juce::dsp::util::snapToZero(xn_2);
-
-    for (auto& yn_1 : Yn_1)
-        juce::dsp::util::snapToZero(yn_1);
-
-    for (auto& yn_2 : Yn_2)
-        juce::dsp::util::snapToZero(yn_2);
+    for (auto v : { &Wn_1, &Wn_2, &Xn_1, &Xn_2, &Yn_1, &Yn_2 })
+        for (auto& element : *v)
+            juce::dsp::util::snapToZero(element);
 }
 
 //==============================================================================
