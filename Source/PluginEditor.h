54a8bd3d4e720ced2b7b5d9f3ffb916691222dea
Nathan Hood <nathanjhood@googlemail.com>
2022-05-31 17:02:59 +0200

    Further custom GUI work
    still bugs on re-draw...

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -33,13 +33,45 @@ private:
     BiquadsAudioProcessor& audioProcessor;
     APVTS& state;
 
-    juce::Slider freqSlider;
-    juce::Slider resSlider;
-    juce::Slider gainSlider;
+    juce::ToggleButton ioButton;
+    juce::Label ioLabel;
+    std::unique_ptr<APVTS::ButtonAttachment> ioAttach;
 
+    juce::Slider freqSlider;
+    juce::Label freqLabel;
     std::unique_ptr<APVTS::SliderAttachment> freqAttach;
+
+    juce::Slider resSlider;
+    juce::Label resLabel;
     std::unique_ptr<APVTS::SliderAttachment> resAttach;
+
+    juce::Slider gainSlider;
+    juce::Label gainLabel;
     std::unique_ptr<APVTS::SliderAttachment> gainAttach;
+
+    juce::ComboBox typeBox;
+    juce::Label typeLabel;
+    std::unique_ptr<APVTS::ComboBoxAttachment> typeAttach;
+
+    juce::ComboBox osBox;
+    juce::Label osLabel;
+    std::unique_ptr<APVTS::ComboBoxAttachment> osAttach;
+
+    juce::Slider outputSlider;
+    juce::Label outputLabel;
+    std::unique_ptr<APVTS::SliderAttachment> outputAttach;
+
+    juce::Slider dryWetSlider;
+    juce::Label dryWetLabel;
+    std::unique_ptr<APVTS::SliderAttachment> dryWetAttach;
+
+    juce::ToggleButton bypassButton;
+    juce::Label bypassLabel;
+    std::unique_ptr<APVTS::ButtonAttachment> bypassAttach;
+
+    juce::ToggleButton displayButton;
+    juce::Label displayLabel;
+    std::unique_ptr<APVTS::ButtonAttachment> displayAttach;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BiquadsAudioProcessorEditor)
